ee67e0296234ea5cfa6c541bb2aebabe42742302
hishamco <hishamco_2007@yahoo.com>
2017-01-14 20:38:16 +0300

    Add PowerBITagHelper

---
+++ src/PowerBI-TagHelper/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PowerBI_TagHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index() => View();
+    }
+}
---
+++ src/PowerBI-TagHelper/TagHelpers/PowerBITagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace PowerBI_TagHelper.TagHelpers
+{
+    [HtmlTargetElement("power-bi", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class PowerBITagHelper : TagHelper
+    {
+        private static readonly string _powerBIAppUrl = "https://app.powerbi.com/";
+
+        public int Height { get; set; } = 600;
+
+        public int Width { get; set; } = 800;
+
+        [HtmlAttributeName("reportCode")]
+        public string ReportCode { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var src = $"{_powerBIAppUrl}view?r={ReportCode}";
+            output.TagName = null;
+            output.Content.SetHtmlContent($@"<iframe width=""{Width}"" height=""{Height}"" src=""{src}"" frameborder=""0"" allowFullScreen=""true""></iframe>");
+        }
+    }
+}
