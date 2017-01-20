40c2895fb38f122569bc96c769136e7c0ba5ff89
hishamco <hishamco_2007@yahoo.com>
2017-01-21 01:15:48 +0300

    ReportCode -> ReportId

--- src/PowerBI-TagHelper/TagHelpers/PowerBITagHelper.cs
+++ src/PowerBI-TagHelper/TagHelpers/PowerBITagHelper.cs
@@ -5,20 +5,20 @@ namespace PowerBI_TagHelper.TagHelpers
     [HtmlTargetElement("power-bi", TagStructure = TagStructure.NormalOrSelfClosing)]
     public class PowerBITagHelper : TagHelper
     {
-        private static readonly string _powerBIAppUrl = "https://app.powerbi.com/";
-
         public int Height { get; set; } = 600;
+
+        [HtmlAttributeName("reportId")]
+        public string ReportId { get; set; }
 
         public int Width { get; set; } = 800;
 
-        [HtmlAttributeName("reportCode")]
-        public string ReportCode { get; set; }
-
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            var src = $"{_powerBIAppUrl}view?r={ReportCode}";
             output.TagName = null;
-            output.Content.SetHtmlContent($@"<iframe width=""{Width}"" height=""{Height}"" src=""{src}"" frameborder=""0"" allowFullScreen=""true""></iframe>");
+
+            const string PowerBIAppUrl = "https://app.powerbi.com/";
+            var embeddUrl = $"{PowerBIAppUrl}view?r={ReportId}";
+            output.Content.SetHtmlContent($@"<iframe width=""{Width}"" height=""{Height}"" src=""{embeddUrl}"" frameborder=""0"" allowFullScreen=""true""></iframe>");
         }
     }
 }
