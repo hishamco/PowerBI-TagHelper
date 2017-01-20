15a5cffb06342ce854c069c2c1b6c7f9b2713a6e
hishamco <hishamco_2007@yahoo.com>
2017-01-21 01:20:18 +0300

    Add 'ToUnixTimestamp' extension method

---
+++ src/PowerBI-TagHelper/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PowerBI_TagHelper.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int ToUnixTimestamp(this DateTime date) =>
+            (int)(date.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+    }
+}
